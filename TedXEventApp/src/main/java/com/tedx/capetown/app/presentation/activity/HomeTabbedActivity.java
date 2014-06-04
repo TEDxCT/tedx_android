5ffd0b18c733a3064a7ba17de1147be576852cea
Richard Cooke <richard.j.e.cooke@gmail.com>
2014-06-04 22:15:37 +0200

    Work on making fragments for the pager. Not working yet.

--- TedXEventApp/src/main/java/com/tedx/capetown/app/presentation/activity/HomeTabbedActivity.java
+++ TedXEventApp/src/main/java/com/tedx/capetown/app/presentation/activity/HomeTabbedActivity.java
@@ -1,43 +1,24 @@
 package com.tedx.capetown.app.presentation.activity;
 
-import java.util.Locale;
-
 import android.app.ActionBar;
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.tedx.capetown.app.R;
+import com.tedx.capetown.app.presentation.adapter.TabSectionsPagerAdapter;
 
+public class HomeTabbedActivity extends ActionBarActivity implements ActionBar.TabListener
+{
 
-public class HomeTabbedActivity extends ActionBarActivity implements ActionBar.TabListener {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
+    TabSectionsPagerAdapter _SectionsPagerAdapter;
+    ViewPager _ViewPager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hometabbed);
 
@@ -46,15 +27,11 @@ public class HomeTabbedActivity extends ActionBarActivity implements ActionBar.T
         actionBar.setDisplayShowHomeEnabled(false);
         actionBar.setDisplayShowTitleEnabled(false);
 
-        // Create the adapter that will return a fragment for each of the three primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+        _SectionsPagerAdapter = new TabSectionsPagerAdapter(getFragmentManager(), getApplicationContext());
+        _ViewPager = (ViewPager) findViewById(R.id.pager);
+        _ViewPager.setAdapter(_SectionsPagerAdapter);
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        // When swiping between different sections, select the corresponding         // tab. We can also use ActionBar.Tab#select() to do this if we have        // a reference to the Tab.
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
+        _ViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
         {
             @Override
             public void onPageSelected(int position)
@@ -63,12 +40,9 @@ public class HomeTabbedActivity extends ActionBarActivity implements ActionBar.T
             }
         });
 
-        // For each of the sections in the app, add a tab to the action bar.
-        // Create a tab with text corresponding to the page title defined by             // the adapter. Also specify this Activity object, which implements             // the TabListener interface, as the callback (listener) for when            // this tab is selected.
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++)
-            actionBar.addTab(actionBar.newTab().setText(mSectionsPagerAdapter.getPageTitle(i)).setTabListener(this));
+        for (int i = 0; i < _SectionsPagerAdapter.getCount(); i++)
+            actionBar.addTab(actionBar.newTab().setText(_SectionsPagerAdapter.getPageTitle(i)).setTabListener(this));
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
@@ -80,9 +54,6 @@ public class HomeTabbedActivity extends ActionBarActivity implements ActionBar.T
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings)
             return true;
@@ -92,9 +63,7 @@ public class HomeTabbedActivity extends ActionBarActivity implements ActionBar.T
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
     {
-        // When the given tab is selected, switch to the corresponding page in
-        // the ViewPager.
-        mViewPager.setCurrentItem(tab.getPosition());
+        _ViewPager.setCurrentItem(tab.getPosition());
     }
 
     @Override
@@ -107,79 +76,4 @@ public class HomeTabbedActivity extends ActionBarActivity implements ActionBar.T
     {
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter
-    {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount()
-        {
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position)
-        {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1);
-                case 1:
-                    return getString(R.string.title_section2);
-                case 2:
-                    return getString(R.string.title_section3);
-            }
-            return null;
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber)
-        {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment()
-        {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            View rootView = inflater.inflate(R.layout.fragment_tabbed, container, false);
-            return rootView;
-        }
-
-    }
 }
---
+++ TedXEventApp/src/main/java/com/tedx/capetown/app/presentation/adapter/TabSectionsPagerAdapter.java
@@ -0,0 +1,86 @@
+package com.tedx.capetown.app.presentation.adapter;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.tedx.capetown.app.R;
+import com.tedx.capetown.app.presentation.activity.AgendaFragment;
+
+import java.util.Locale;
+
+public class TabSectionsPagerAdapter extends FragmentPagerAdapter
+{
+
+    Context _context;
+
+    public TabSectionsPagerAdapter(FragmentManager fm, Context context)
+    {
+        super(fm);
+        _context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position)
+    {
+        if (position == 0)
+           return AgendaFragment.newInstance();
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount()
+    {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position)
+    {
+        Locale l = Locale.getDefault();
+        switch (position) {
+            case 0:
+                return _context.getString(R.string.title_section1);
+            case 1:
+                return _context.getString(R.string.title_section2);
+            case 2:
+                return _context.getString(R.string.title_section3);
+        }
+        return null;
+    }
+
+
+
+
+
+    public static class PlaceholderFragment extends Fragment
+    {
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public static PlaceholderFragment newInstance(int sectionNumber)
+        {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment()
+        {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View rootView = inflater.inflate(R.layout.fragment_tabbed, container, false);
+            return rootView;
+        }
+    }
+
+
+}
